62ce30f3f8b53c18a5c518025a39ee069a6ff34e
yhira <info@nelog.jp>
2019-04-09 15:32:29 +0900

    付箋ボックスのリファクタリング

--- src/block/sticky-box/block.js
+++ src/block/sticky-box/block.js
@@ -7,6 +7,7 @@
 
 import {THEME_NAME, BLOCK_CLASS} from '../../helpers.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classnames from 'classnames';
 
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
@@ -14,6 +15,19 @@ const { InnerBlocks, RichText, InspectorControls } = wp.editor;
 const { PanelBody, SelectControl, BaseControl } = wp.components;
 const { Fragment } = wp.element;
 const DEFAULT_MSG = __( 'こちらをクリックして設定変更。この入力は公開ページで反映されません。', THEME_NAME );
+
+//classの取得
+function getClasses(style) {
+  const classes = classnames(
+    {
+      [ 'blank-box' ]: true,
+      [ 'sticky' ]: true,
+      [ style ]: !! style,
+      [ 'block-box' ]: true,
+    }
+  );
+  return classes;
+}
 
 registerBlockType( 'cocoon-blocks/sticky-box', {
 
@@ -25,26 +39,16 @@ registerBlockType( 'cocoon-blocks/sticky-box', {
   attributes: {
     content: {
       type: 'string',
-      source: 'html',
-      selector: 'div',
       default: DEFAULT_MSG,
     },
     style: {
       type: 'string',
-      default: 'blank-box sticky',
+      default: '',
     },
   },
 
   edit( { attributes, setAttributes } ) {
-    const { content, style, alignment } = attributes;
-
-    function onChange(event){
-      setAttributes({style: event.target.value});
-    }
-
-    function onChangeContent(newContent){
-      setAttributes( { content: newContent } );
-    }
+    const { content, style } = attributes;
 
     return (
       <Fragment>
@@ -57,23 +61,23 @@ registerBlockType( 'cocoon-blocks/sticky-box', {
               onChange={ ( value ) => setAttributes( { style: value } ) }
               options={ [
                 {
-                  value: 'blank-box sticky',
+                  value: '',
                   label: __( '灰色', THEME_NAME ),
                 },
                 {
-                  value: 'blank-box sticky st-yellow',
+                  value: 'st-yellow',
                   label: __( '黄色', THEME_NAME ),
                 },
                 {
-                  value: 'blank-box sticky st-red',
+                  value: 'st-red',
                   label: __( '赤色', THEME_NAME ),
                 },
                 {
-                  value: 'blank-box sticky st-blue',
+                  value: 'st-blue',
                   label: __( '青色', THEME_NAME ),
                 },
                 {
-                  value: 'blank-box sticky st-green',
+                  value: 'st-green',
                   label: __( '緑色', THEME_NAME ),
                 },
               ] }
@@ -82,7 +86,7 @@ registerBlockType( 'cocoon-blocks/sticky-box', {
           </PanelBody>
         </InspectorControls>
 
-        <div className={attributes.style + BLOCK_CLASS}>
+        <div className={ getClasses(style) }>
           <span className={'box-block-msg'}>
             <RichText
               value={ content }
@@ -96,9 +100,9 @@ registerBlockType( 'cocoon-blocks/sticky-box', {
   },
 
   save( { attributes } ) {
-    const { content } = attributes;
+    const { content, style } = attributes;
     return (
-      <div className={attributes.style + BLOCK_CLASS}>
+      <div className={ getClasses(style) }>
         <InnerBlocks.Content />
       </div>
     );
