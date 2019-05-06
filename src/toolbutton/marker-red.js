1b6be77e596a75771c7aa2df1d4a483f6945fce5
yhira <info@nelog.jp>
2019-05-07 00:19:01 +0900

    マーカードロップダウンリストのリファクタリング

--- src/toolbutton/marker-red.js
+++ src/toolbutton/marker-red.js
@@ -5,37 +5,31 @@
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
 
+import {THEME_NAME, MarkerToolbarButton } from '../helpers.js';
 const { Fragment } = wp.element;
 const { __ } = wp.i18n;
 const { registerFormatType, toggleFormat } = wp.richText;
 const { RichTextShortcut, RichTextToolbarButton } = wp.editor;
-const THEME_NAME = 'cocoon';
 const FORMAT_TYPE_NAME = 'cocoon-blocks/marker-red';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const TITLE = __( '赤色マーカー', THEME_NAME );
 
 registerFormatType( FORMAT_TYPE_NAME, {
-  title: __( '赤色マーカー', THEME_NAME ),
+  title: TITLE,
   tagName: 'span',
   className: 'marker-red',
-  // edit( { isActive, value, onChange } ) {
-  //   const onToggle = () => onChange( toggleFormat( value, { type: FORMAT_TYPE_NAME } ) );
+  edit({isActive, value, onChange}){
+    const onToggle = () => onChange(toggleFormat(value,{type:FORMAT_TYPE_NAME}));
 
-  //   return (
-  //     <Fragment>
-  //       <RichTextShortcut
-  //         type='primary'
-  //         character=''
-  //         onUse={ onToggle }
-  //       />
-  //       <RichTextToolbarButton
-  //         title={ __( '赤色マーカー', THEME_NAME ) }
-  //         icon={ <FontAwesomeIcon icon="highlighter" /> }
-  //         onClick={ onToggle }
-  //         isActive={ isActive }
-  //         shortcutType='primary'
-  //         shortcutCharacter=''
-  //       />
-  //     </Fragment>
-  //   );
-  // },
+    return (
+      <Fragment>
+        <MarkerToolbarButton
+          icon={<FontAwesomeIcon icon="highlighter" />}
+          title={<span className="marker-red">{TITLE}</span>}
+          onClick={ onToggle }
+          isActive={ isActive }
+        />
+      </Fragment>
+    );
+  }
 } );
