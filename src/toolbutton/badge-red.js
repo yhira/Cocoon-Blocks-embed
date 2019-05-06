0a79eb2bdaadc087d465ac2b020777de482f72c4
yhira <info@nelog.jp>
2019-05-07 00:49:32 +0900

    バッジドロップダウンリストのリファクタリング

--- src/toolbutton/badge-red.js
+++ src/toolbutton/badge-red.js
@@ -5,13 +5,29 @@
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
 
+import {THEME_NAME, BadgeToolbarButton } from '../helpers.js';
+const { Fragment } = wp.element;
 const { __ } = wp.i18n;
-const { registerFormatType } = wp.richText;
-const THEME_NAME = 'cocoon';
+const { registerFormatType, toggleFormat } = wp.richText;
 const FORMAT_TYPE_NAME = 'cocoon-blocks/badge-red';
+const TITLE = __( '赤色', THEME_NAME );
 
 registerFormatType( FORMAT_TYPE_NAME, {
-  title: __( '赤色', THEME_NAME ),
+  title: TITLE,
   tagName: 'span',
   className: 'badge-red',
+  edit({isActive, value, onChange}){
+    const onToggle = () => onChange(toggleFormat(value,{type:FORMAT_TYPE_NAME}));
+
+    return (
+      <Fragment>
+        <BadgeToolbarButton
+          icon="tag"
+          title={<span className="badge-red">{TITLE}</span>}
+          onClick={ onToggle }
+          isActive={ isActive }
+        />
+      </Fragment>
+    );
+  }
 } );
