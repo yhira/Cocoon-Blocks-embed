7453a84cbbc41b9d6b0672418aae71e1886c060a
yhira <info@nelog.jp>
2019-05-14 16:55:15 +0900

    ショートコード6種類追加

--- src/toolbutton/dropdown-shortcodes.js
+++ src/toolbutton/dropdown-shortcodes.js
@@ -15,27 +15,100 @@ const FORMAT_TYPE_NAME = 'cocoon-blocks/shortcodes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { orderBy } from 'lodash';
 
-var name = 'shortcode-ad';
-var title = __( '広告', THEME_NAME );
-var formatType = 'cocoon-blocks/' + name;
-registerFormatType( formatType, {
-  title: title,
-  tagName: name,
-  className: null,
-  edit({value, onChange}){
-    const onToggle = () => onChange( insert( value, '[ad]', value.start, value.end ) );
+//ショートコード作成関数
+function registerShortcodeFormatType(name, title, code, icon) {
+  var formatType = 'cocoon-blocks/' + name;
+  registerFormatType( formatType, {
+    title: title,
+    tagName: name,
+    className: null,
+    edit({value, onChange}){
+      const onToggle = () => onChange( insert( value, code, value.start, value.end ) );
 
-    return (
-      <Fragment>
-        <ShortcodeToolbarButton
-          icon={<FontAwesomeIcon icon={['fas', 'ad']} />}
-          title={<span className={name}>{title}</span>}
-          onClick={ onToggle }
-        />
-      </Fragment>
-    );
-  }
-} );
+      return (
+        <Fragment>
+          <ShortcodeToolbarButton
+            icon={<FontAwesomeIcon icon={icon} />}
+            title={<span className={name}>{title}</span>}
+            onClick={ onToggle }
+          />
+        </Fragment>
+      );
+    }
+  } );
+}
+//広告
+registerShortcodeFormatType(
+  'shortcode-ad',
+  __( '広告', THEME_NAME ),
+  '[ad]',
+  ['fas', 'ad']
+);
+//新着記事一覧
+registerShortcodeFormatType(
+  'shortcode-new-list',
+  __( '新着記事一覧', THEME_NAME ),
+  '[new_list count="5" type="default" cats="all" children="0" post_type="post"]',
+  ['fas', 'th-list']
+);
+//人気記事一覧
+registerShortcodeFormatType(
+  'shortcode-popular-list',
+  __( '人気記事一覧', THEME_NAME ),
+  '[popular_list days="all" rank="0" pv="0" count="5" type="default" cats="all"]',
+  ['fas', 'th-list']
+);
+//プロフィールボックス
+registerShortcodeFormatType(
+  'shortcode-profile',
+  __( 'プロフィールボックス', THEME_NAME ),
+  '[author_box label="' + __( 'この記事を書いた人', THEME_NAME ) + ']',
+  ['fas', 'user-circle']
+);
+//Amazonリンク
+registerShortcodeFormatType(
+  'shortcode-amazon',
+  __( 'Amazonリンク', THEME_NAME ),
+  '[amazon asin="ASIN" kw="' + __( 'キーワード', THEME_NAME ) + '"]',
+  ['fab', 'amazon']
+);
+//Amazonリンク（商品名変更）
+registerShortcodeFormatType(
+  'shortcode-amazon-title',
+  __( 'Amazonリンク（商品名変更）', THEME_NAME ),
+  '[amazon asin="ASIN" title="' + __( '商品名', THEME_NAME ) + '" kw="' + __( 'キーワード', THEME_NAME ) + '" amazon=0 rakuten=0 yahoo=0]',
+  ['fab', 'amazon']
+);
+
+// //XXXXXXXXX
+// registerShortcodeFormatType(
+//   'shortcode-',
+//   __( 'XXXXXXXXX', THEME_NAME ),
+//   '',
+//   ['fas', '']
+// );
+
+// var name = 'shortcode-ad';
+// var title = __( '広告', THEME_NAME );
+// var formatType = 'cocoon-blocks/' + name;
+// registerFormatType( formatType, {
+//   title: title,
+//   tagName: name,
+//   className: null,
+//   edit({value, onChange}){
+//     const onToggle = () => onChange( insert( value, '[ad]', value.start, value.end ) );
+
+//     return (
+//       <Fragment>
+//         <ShortcodeToolbarButton
+//           icon={<FontAwesomeIcon icon={['fas', 'ad']} />}
+//           title={<span className={name}>{title}</span>}
+//           onClick={ onToggle }
+//         />
+//       </Fragment>
+//     );
+//   }
+// } );
 
 registerFormatType( FORMAT_TYPE_NAME, {
   title: __( 'アフィリエイト', THEME_NAME ),
