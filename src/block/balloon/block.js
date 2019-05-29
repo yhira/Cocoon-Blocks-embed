f6e1390d4423b35402f4c07f93d0a122566d0c24
yhira <info@nelog.jp>
2019-05-30 00:39:09 +0900

    吹き出しブロックの統合

--- src/block/balloon/block.js
+++ src/block/balloon/block.js
@@ -11,17 +11,17 @@ import classnames from 'classnames';
 
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { InnerBlocks, RichText, InspectorControls } = wp.editor;
-const { PanelBody, SelectControl, BaseControl } = wp.components;
+const { InnerBlocks, RichText, MediaUpload, InspectorControls } = wp.editor;
+const { Button, PanelBody, SelectControl, BaseControl } = wp.components;
 const { Fragment } = wp.element;
 const DEFAULT_NAME = __( '未入力', THEME_NAME );
 
-registerBlockType( 'cocoon-blocks/balloon-box-2', {
+registerBlockType( 'cocoon-blocks/balloon-ex-box-1', {
 
   title: __( '吹き出し', THEME_NAME ),
   icon: <FontAwesomeIcon icon={['far', 'comment']} />,
   category: THEME_NAME + '-block',
-  description: __( '登録されている吹き出しを挿入できます。', THEME_NAME ),
+  description: __( '登録されている吹き出しのオプションを変更できます。', THEME_NAME ),
   keywords: [ 'balloon', 'box' ],
 
   attributes: {
@@ -53,10 +53,14 @@ registerBlockType( 'cocoon-blocks/balloon-box-2', {
       type: 'string',
       default: 'cb',
     },
+    iconid: {
+      type: 'number',
+      default: 0,
+    },
   },
 
   edit( { attributes, setAttributes } ) {
-    var { name, index, id, icon, style, position, iconstyle } = attributes;
+    var { name, index, id, icon, style, position, iconstyle, iconid } = attributes;
     //新規作成時
     if (!icon && index == '0' && speechBaloons[0]) {
         id = speechBaloons[0].id;
@@ -85,6 +89,17 @@ registerBlockType( 'cocoon-blocks/balloon-box-2', {
       }
     }
 
+    const renderIcon = ( obj ) => {
+      // console.log(icon);
+      // console.log(speechBaloons[index].icon);
+      // console.log((icon === speechBaloons[index].icon) ? icon : speechBaloons[index].icon);
+      return (
+        <Button className="image-button" onClick={ obj.open } style={ { padding: 0 } }>
+          <img src={ icon ? icon : speechBaloons[index].icon } alt={icon ? '' : speechBaloons[index].name} className={ `speech-icon-image wp-image-${ iconid }` } />
+        </Button>
+      );
+    };
+
     //console.log(speechBaloons);
     var balloons = [];
     speechBaloons.map((balloon, index) => {
@@ -95,7 +110,6 @@ registerBlockType( 'cocoon-blocks/balloon-box-2', {
           label: balloon.title,
         });
       }
-
     });
     //console.log(balloons);
 
@@ -118,6 +132,70 @@ registerBlockType( 'cocoon-blocks/balloon-box-2', {
               options={ balloons }
             />
 
+            <SelectControl
+              label={ __( '吹き出しスタイル', THEME_NAME ) }
+              value={ style }
+              onChange={ ( value ) => setAttributes( { style: value } ) }
+              options={ [
+                {
+                  value: 'stn',
+                  label: __( 'デフォルト', THEME_NAME ),
+                },
+                {
+                  value: 'flat',
+                  label: __( 'フラット', THEME_NAME ),
+                },
+                {
+                  value: 'line',
+                  label: __( 'LINE風', THEME_NAME ),
+                },
+                {
+                  value: 'think',
+                  label: __( '考え事', THEME_NAME ),
+                },
+              ] }
+            />
+
+            <SelectControl
+              label={ __( '人物位置', THEME_NAME ) }
+              value={ position }
+              onChange={ ( value ) => setAttributes( { position: value } ) }
+              options={ [
+                {
+                  value: 'l',
+                  label: __( '左', THEME_NAME ),
+                },
+                {
+                  value: 'r',
+                  label: __( '右', THEME_NAME ),
+                },
+              ] }
+            />
+
+            <SelectControl
+              label={ __( 'アイコンスタイル', THEME_NAME ) }
+              value={ iconstyle }
+              onChange={ ( value ) => setAttributes( { iconstyle: value } ) }
+              options={ [
+                {
+                  value: 'sn',
+                  label: __( '四角（枠線なし）', THEME_NAME ),
+                },
+                {
+                  value: 'sb',
+                  label: __( '四角（枠線あり）', THEME_NAME ),
+                },
+                {
+                  value: 'cn',
+                  label: __( '丸（枠線なし）', THEME_NAME ),
+                },
+                {
+                  value: 'cb',
+                  label: __( '丸（枠線あり）', THEME_NAME ),
+                },
+              ] }
+            />
+
           </PanelBody>
         </InspectorControls>
 
@@ -125,10 +203,15 @@ registerBlockType( 'cocoon-blocks/balloon-box-2', {
           className={ getBalloonClasses(id, style, position, iconstyle) }>
           <div className="speech-person">
             <figure className="speech-icon">
-              <img
-                src={icon}
-                alt={name}
-                className="speech-icon-image"
+              <MediaUpload
+                onSelect={ ( media ) => {
+                  let newicon = !! media.sizes.thumbnail ? media.sizes.thumbnail.url : media.url;
+                  //console.log(newicon);
+                  setAttributes( { icon: newicon, iconid: media.id } );
+                } }
+                type="image"
+                value={ iconid }
+                render={ renderIcon }
               />
             </figure>
             <div className="speech-name">
@@ -156,8 +239,8 @@ registerBlockType( 'cocoon-blocks/balloon-box-2', {
           <div className="speech-person">
             <figure className="speech-icon">
               <img
-                src={icon}
-                alt={name}
+                src={ icon }
+                alt={ name }
                 className="speech-icon-image"
               />
             </figure>
