5866a41dd5f49397739a6354dd53eeff1d7d20a0
yhira <info@nelog.jp>
2019-07-13 17:51:44 +0900

    ブログカードブロックの不具合修正

--- src/block/blogcard/block.js
+++ src/block/blogcard/block.js
@@ -113,7 +113,7 @@ registerBlockType( 'cocoon-blocks/blogcard', {
     return (
       <div className={attributes.style}>
           {"\n"}<RichText.Content
-            value={ content.replace(/<\/p><p>/, '</p>\n<p>') }
+            value={ content.replace(/<p> /g, '<p>').replace(/ <\/p>/g, '</p>').replace(/<\/p><p>/g, '</p>\n<p>') }
             multiline={"p"}
           />{"\n"}
       </div>
